6edd3789e2f264e3ae2bb64a5c37f25ec12eef7b
Madigela Maninder <mmaninde@opentext.com>
2021-07-30 21:26:55 +0530

    UI Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'urlShortnerUI';
+  url="/dm/URLShortner/getId";
+  shortUrl:string;
+  constructor(private httpClient:HttpClient){}
+
+  getShortUrl(form:NgForm){
+    console.log(form);
+    const value = form.value;
+   return this.httpClient.post(this.url,value.url,{responseType:'json' ,observe:'response'}).toPromise().then(response=>{
+      this.shortUrl="http://localhost:8081/URLShortner/"+response.body.toString();
+    });
+    
+
+  }
 }
